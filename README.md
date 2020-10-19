aa57d2bfdcc463ef7ba8441d0b11ca1ba9002c1a
nightli <1658484908@qq.com>
2020-10-19 22:22:00 +0800

    docker 初始化脚本

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # docker_init_script
 ubuntu docker init scropt
+
+docker初始化
+国内使用dokcer 更新source.list，pip源为国内源脚本提高下载速度  
+ubuntu16.04 使用   
+ubuntu18.04需改源地址
---
+++ ubuntu16.04_init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+rm /etc/apt/sources.list
+
+echo '
+deb http://mirrors.ustc.edu.cn/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse
+deb http://mirrors.ustc.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial main restricted universe multiverse
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial-proposed main restricted universe multiverse
+deb-src http://mirrors.ustc.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
+    ' > /etc/apt/sources.list
+
+apt-get update
+
+apt-get install -y vim
+
+pipconf_dir="~/.pip/"
+
+if [ ! -d "$pipconf_dir" ];
+then
+	mkdir -p ~/.pip
+	echo '
+	[global]
+	index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+	' > ~/.pip/pip.conf;
+fi
+echo 'docker init finish'
