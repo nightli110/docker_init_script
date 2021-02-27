eb85e8ea5a24dd52ad6aa80606fca9336f0ede6c
nightli <1658484908@qq.com>
2021-02-27 13:26:04 +0800

    add ubuntu18.04 init

---
+++ ubuntu18.04_init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+rm /etc/apt/sources.list
+
+rm  -rf /etc/apt/sources.list.d/*
+
+echo '
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+    ' > /etc/apt/sources.list
+
+apt-get update
+
+apt-get install -y vim wget  python-qt4
+
+pipconf_dir="~/.pip/"
+
+if [ ! -d "$pipconf_dir" ];
+then
+	mkdir -p ~/.pip
+	echo -e '
+    [global]
+    \nindex-url = https://pypi.tuna.tsinghua.edu.cn/simple
+	' > ~/.pip/pip.conf;
+fi
+echo 'docker init finish'
